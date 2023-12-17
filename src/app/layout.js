c3391db9cbe50c5a17601f63f12e1b0eae5586c9
RaktimBanerje <braktim99@gmail.com>
2023-12-17 21:46:09 +0530

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <link rel="manifest" href="manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
       <body className={inter.className}>{children}</body>
     </html>
   )
